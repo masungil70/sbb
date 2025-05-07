6e2a808441343c42835244741d5d619b6757561a
kosa <kosa@kosa.com>
2025-05-07 16:25:20 +0900

    질문 목록 출력시 최신 글 기준으로 조회 기능 구현 정렬 방법 추가함

--- src/main/java/org/kosa/sbb/question/QuestionService.java
+++ src/main/java/org/kosa/sbb/question/QuestionService.java
@@ -1,6 +1,7 @@
 package org.kosa.sbb.question;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,6 +10,7 @@ import org.kosa.sbb.answer.Answer;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -29,7 +31,14 @@ public class QuestionService {
 	}
 
 	public Page<Question> getList(int page) {
-		Pageable pageable = PageRequest.of(page, 10);
+		//정렬 객체 생성 
+		List<Sort.Order> sorts = new ArrayList<Sort.Order>();
+		
+		//정려 필드와 방법을 설정함 
+		sorts.add(Sort.Order.desc("createDate"));
+
+		//페이지 요청 객체에 정렬 객체 추가함 
+		Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
 		return questionRepository.findAll(pageable);
 	}
 	
