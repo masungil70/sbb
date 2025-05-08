789603599d59915402c82a949b8a8c069891764a
kosa <kosa@kosa.com>
2025-05-08 10:40:29 +0900

    페이징 네비게이션 바 부분 처리를 변경 함

--- src/main/java/org/kosa/sbb/question/QuestionController.java
+++ src/main/java/org/kosa/sbb/question/QuestionController.java
@@ -1,13 +1,10 @@
 package org.kosa.sbb.question;
 
-import java.util.List;
-
 import org.kosa.sbb.answer.AnswerForm;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +13,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
+/*
+ * 80% spring boot 제공하는 클래스 
+ * 20% jdk api 사용(util, collection, format)  
+ * 경우에 따라서 제3자가 제공하는 클래스 (라이브러리) -> 직접 사용하는 것 
+ *      -> wrapper class로 작성하는 방법  
+ *      
+ * 
+ * spring boot 에서 제공하는 Paging 관한 클래스 -> nav Paging 부분
+ *    
+ * 
+ */
 
 @Controller
 @RequiredArgsConstructor
@@ -29,6 +38,13 @@ public class QuestionController {
 			@RequestParam(value="page", defaultValue = "0") int page) {
 		Page<Question> paging = questionService.getList(page);
 		model.addAttribute("paging", paging);
+		
+		int startPage = ((page) / 10) * 10;
+		int endPage = ((page) / 10) * 10 + 9;
+		if (endPage > paging.getTotalPages()) endPage = paging.getTotalPages();
+		model.addAttribute("startPage", startPage);
+		model.addAttribute("endPage", endPage);
+
 		return "question_list";
 	}
 	
